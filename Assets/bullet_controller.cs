0f360299a267fcb18aead2618ca87c254529a8e5
Sigf <seb.hutt@gmail.com>
2015-06-28 20:21:15 -0700

    trying to make the pattern repeatable

--- Assets/bullet_controller.cs
+++ Assets/bullet_controller.cs
@@ -4,13 +4,21 @@ using System.Collections;
 public class bullet_controller : MonoBehaviour {
 
 	public float speed = 10.0f;
+	public Camera camera;
 	private Vector3 moveDirection;
-	
+
 	void Start () {
 		moveDirection = transform.rotation * new Vector3(1.0f, 0.0f, 0.0f);
 	}
 
 	void Update () {
+
+		Vector3 pos = this.camera.WorldToViewportPoint(transform.position);
+
 		transform.Translate(moveDirection * Time.deltaTime * this.speed);
+
+		if (pos.x < 0.0f || pos.x > Screen.width || pos.y < 0.0f || pos.y > Screen.height) {
+			Destroy(gameObject);
+		}
 	}
 }
