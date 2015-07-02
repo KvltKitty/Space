4ab96ac1b683c57d5cdf19bf20e911b1de59b3a4
KvltKitty <jherzenach@mail.csuchico.edu>
2015-07-01 17:47:27 -0700

    SeemsToWorkBetter

--- SPACE!/Assets/Scripts/playerController.cs
+++ SPACE!/Assets/Scripts/playerController.cs
@@ -5,12 +5,14 @@ public class playerController : MonoBehaviour {
 	Vector3 viewportPosition;
 	Vector3 amountToMove;
 	Camera _camera;
+	BoxCollider _collider;
 	public playerStats _stats;
 	// Use this for initialization
 	void Start () {
 		_camera = Camera.main;
 		amountToMove = Vector3.zero;
 		_stats = new playerStats();
+		_collider = transform.GetComponent<BoxCollider>();
 	}
 	
 
@@ -18,23 +20,31 @@ public class playerController : MonoBehaviour {
 	public void Move(controllerState state){
 		amountToMove = Vector3.zero;
 		viewportPosition = _camera.WorldToViewportPoint(transform.position);
-		if(state.horizontal > 0.0f){
-			if(viewportPosition.x + (state.horizontal * _stats.speed) < 1.0f){
+		if(state.horizontal > 0.0f) //if going right
+		{
+			if(viewportPosition.x + (state.horizontal * _stats.speed) < 1.0f)
+			{
 				amountToMove.x = (state.horizontal * _stats.speed);
 			}
 		}
-		else if(state.horizontal < 0.0f){
-			if(viewportPosition.x + (state.horizontal * _stats.speed) > 0.0f){
+		else if(state.horizontal < 0.0f) //if going left
+		{
+			if(viewportPosition.x  + (state.horizontal * _stats.speed) > 0.0f)
+			{
 				amountToMove.x = (state.horizontal * _stats.speed);
 			}
 		}
-		if(state.vertical > 0.0f){
-			if(viewportPosition.y + (state.vertical * _stats.speed) < 1.0f){
+		if(state.vertical > 0.0f) //if going up
+		{
+			if(viewportPosition.y  + (state.vertical * _stats.speed) < 1.0f)
+			{
 				amountToMove.y = (state.vertical * _stats.speed);
 			}
 		}
-		else if(state.vertical < 0.0f){
-			if(viewportPosition.y + (state.vertical * _stats.speed) > 0.0f){
+		else if(state.vertical < 0.0f) //if going down
+		{
+			if(viewportPosition.y  + (state.vertical * _stats.speed) > 0.0f)
+			{
 				amountToMove.y = (state.vertical * _stats.speed);
 			}
 		}
@@ -45,4 +55,6 @@ public class playerController : MonoBehaviour {
 	public void Fire(controllerState state){
 
 	}
+
+
 }
